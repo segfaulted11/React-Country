b1beffa0b9a4e24252aec23e48443b0921727792
Seg Fault <segfaulted11@gmail.com>
2026-04-22 12:20:24 +0600

    lift up state and argument from children and parameter from parent

--- src/Components/Countries.jsx
+++ src/Components/Countries.jsx
@@ -1,10 +1,23 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from './Country';
 
 const Countries = ({countriesDataProps}) => {
 
     const countriesData = use(countriesDataProps);// entire object
     const {countries} = countriesData;//array consisting 250 objects
+
+    //lift up state -> 
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = (countryNameAsparameter) =>{
+        console.log(`you have clicked ${countryNameAsparameter} country.`);
+    }
+    //create the state in the parent Component(Countries.jsx). 
+    //create the stateHandling function in the parent Component.
+    //send that function as a props to the children component.
+    //deconstruct the props in the children (Country.jsx) component.
+    //use that function in the children component's button to an event. if theres already an event in that button, you can use arrow function for wrapping everything together.  
+
 
     return (
 
@@ -17,8 +30,8 @@ const Countries = ({countriesDataProps}) => {
             } */}
 
             <div className='grid grid-cols-3 gap-3'>
-            {//same as the previous but this time it sents this to another component for rendidng insteading rendiring directly from here.
-                countries.map(eachObj => <Country eachObj={eachObj}></Country> )
+            {//same as the previous but this time it sents this to another component for rendidng insteading rendiring directly from here. parent sending it as props to the child.
+                countries.map(eachObj => <Country eachObj={eachObj} key={eachObj.cca3.cca3} handleVisitedCountries={handleVisitedCountries}></Country> )
             }
             </div>
         </div>
--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Country = ({eachObj}) => {
+const Country = ({eachObj, handleVisitedCountries}) => {
 
     //deconstrcuturing
     const {name, ccn3, currencies, captial, region, languages, area, cca3, population, continents, flags } = eachObj;
@@ -29,11 +29,11 @@ const Country = ({eachObj}) => {
     //       setVisited(visited ? false : true)
     // }
 
+    
     // way - 3 (logical not operator)
     const handleVisited = () => {
         setVisited(!visited);
     }
-
 
     return (
         <div className='bg-red-200 mb-4'>
@@ -43,9 +43,15 @@ const Country = ({eachObj}) => {
             <p>Area : {area.area}
                 {area.area>30000 ? " (Big Country)" : " (Small Country)"}
             </p>
-            <button onClick={handleVisited} className={`p-3 hover:cursor-pointer
+           <button onClick={()=> {
+            handleVisited();
+            handleVisitedCountries(name.common);//sending the country name as the argument and from the parent component,event handling function 'handleVisitedCountries' is going recieve that. , so a particular country's button will be clicked, its gonna show that country's name.
+           }//wrappping multiple event handling functions using arrow function
+            } 
+            className={`p-3 hover:cursor-pointer
                 ${visited ? "text-white bg-red-400" : "bg-purple-500"}
-                `}>
+                `}
+                > 
                 {visited ? "Visited" : "Not Visited"}
             </button>
         </div>
